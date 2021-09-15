04448cedacb4aa5e75e10dbcc578d5e529494f1f
Japhet Elias <japhet9@gmail.com>
2021-09-15 23:40:10 +0000

    implemented function and test cases

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+
+  for (let index = 0; index < sentence.length; index++) {
+    if (results[sentence[index]]) {
+      results[sentence[index]].push(index);
+    } else {
+      results[sentence[index]] = [index];
+    }
+  }
+  return results;
+};
+
+const posResults = letterPositions("lighthouse in the house");
+assertArraysEqual(posResults.h, [3, 5, 15, 18]);
+assertArraysEqual(posResults.l, [0]);
+assertArraysEqual(posResults.u, [7, 20]);
+assertArraysEqual(posResults.n, [12]);
