d7ced2779deab696025a95bf357ee72d994ea95f
Japhet Elias <japhet9@gmail.com>
2021-09-15 19:56:02 +0000

    count everything in allItems

--- countOnly.js
+++ countOnly.js
@@ -10,8 +10,19 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  console.log(results);
+
   return results;
 }
+
 
 const firstNames = [
   "Karl",
