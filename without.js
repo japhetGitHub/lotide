d0ac78f3997d853a38f2f6befc1543ee59933dda
Japhet Elias <japhet9@gmail.com>
2021-09-15 06:06:25 +0000

    implemented 'without' function, added additional test cases, and linted

--- without.js
+++ without.js
@@ -18,8 +18,21 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const without = function() {
+const without = function(source, itemsToRemove) {
+  let newArr = [];
+  for (const val of source) {
+    newArr.push(val);
+  }
 
+  for (let i = 0; i < itemsToRemove.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (newArr[j] === itemsToRemove[i]) {
+        newArr.splice(j, 1);
+      }
+    }
+  }
+
+  return newArr;
 };
 
 
@@ -27,6 +40,10 @@ const without = function() {
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // true
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // true
+assertArraysEqual(without(["1", "2", "3"], [1, 2]), ["1", "2", "3"]); // true
+assertArraysEqual(without(["1", "2", "3"], []), ["1", "2", "3"]); // true
+assertArraysEqual(without([], [1, 2, 3]), []); // true
+
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
