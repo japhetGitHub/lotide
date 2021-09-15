d194d44939a38e67acdeff500069ef6c2f19bed9
Japhet Elias <japhet9@gmail.com>
2021-09-15 19:57:44 +0000

    only count allItems elements within itemsToCount object

--- countOnly.js
+++ countOnly.js
@@ -12,13 +12,15 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]){
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
-  console.log(results);
+  console.log(results);  
 
   return results;
 }
