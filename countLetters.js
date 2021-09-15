1b5fad0085fe33caaac6477d396e1123b3aed79f
Japhet Elias <japhet9@gmail.com>
2021-09-15 22:07:23 +0000

    implemented countLetters function and added asserts

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const countLetters = function(sentence) {
+  let letters = {};
+
+  for (const char of sentence) {
+    if (char === " ") { // skips if character is a space
+      continue;
+    } else {
+      if (letters[char]) { //checks if character already exists in letters object
+        letters[char] += 1; // adds to existing character count in object
+      } else {
+        letters[char] = 1; // starts character count in object
+      }
+    }
+  }
+
+  return letters;
+};
+
+// console.log(countLetters("lighthouse in the house"));
+const result = countLetters("Hello LHL World");
+assertEqual(result["e"], 1);
+assertEqual(result["l"], 3);
+assertEqual(result["o"], 2);
+assertEqual(result["r"], 1);
+assertEqual(result["d"], 1);
+assertEqual(result["H"], 2);
+assertEqual(result["L"], 2);
+assertEqual(result["W"], 1);
+
