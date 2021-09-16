be25044bfd5bc7bb29115d97e22e5f55159f5490
Japhet Elias <japhet9@gmail.com>
2021-09-16 00:00:29 +0000

    implemented the function using a for...in statmenet

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,15 +7,26 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
-
+  let foundKey;
+  for (const key in object) {
+    if (object[key] === value) {
+      foundKey = key;
+      return foundKey;
+    }
+  }
+  return foundKey;
 };
 
 // Test Assertions
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama: "The Wire",
+  fantasy: "Game of Thrones",
+  Crime: "The Wire"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Game of Thrones"), "fantasy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
