d49a62a39f96320c1c566ab1fc5c1abb658c01a5
Japhet Elias <japhet9@gmail.com>
2021-09-16 02:19:44 +0000

    started eqObjects function definition

---
+++ eqObjects.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
