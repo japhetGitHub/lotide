8bd95a3b612470487c340ee711bc401e696c749c
Japhet Elias <japhet9@gmail.com>
2021-09-21 18:23:17 +0000

    modularized findKey and converted tests to Mocha and Chai

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = function(obj, callback) {
   for (const key in obj) {
     if (callback(obj[key])) {
@@ -14,14 +6,4 @@ const findKey = function(obj, callback) {
   }
 };
 
-//Test Cases
-testObj = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-assertEqual(findKey(testObj, x => x.stars === 2), "noma");
-assertEqual(findKey(testObj, x => x.stars === 10), undefined);
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe("#findKey", () => {
+  it("should return the first property in object whose value satisfies callback", () => {
+    const inputObj = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    const callback = x => x.stars === 2;
+    const expectedOutput = "noma";
+    assert.strictEqual(findKey(inputObj, callback), expectedOutput);
+  });
+
+  it("should return undefined if no properties in object satisfy callback", () => {
+    const inputObj = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    const callback = x => x.stars === 10;
+    assert.isUndefined(findKey(inputObj, callback));
+  })
+});
