aa4091a37e181c49a8312aa215f0699439efeddb
Japhet Elias <japhet9@gmail.com>
2021-09-21 05:48:02 +0000

    refactored middleTest to use Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,38 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const chai = require('chai');
+const assert = chai.assert;
+
 const middle = require('../middle');
 
-//TEST CASES
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("check that single element array returns empty array as middle", () => {
+    const inputArr = [1];
+    const expectedOutput = [];
+    assert.deepEqual(middle(inputArr), expectedOutput);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("check that two element array returns empty array as middle", () => {
+    const inputArr = [1, 2];
+    const expectedOutput = [];
+    assert.deepEqual(middle(inputArr), expectedOutput);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("check that array with odd number of elements returns only the single middle element in an array", () => {
+    const inputArr1 = [1, 2, 3];
+    const expectedOutput1 = [2];
+    assert.deepEqual(middle(inputArr1), expectedOutput1);
+    
+    const inputArr2 = [1, 2, 3, 4, 5];
+    const expectedOutput2 = [3];
+    assert.deepEqual(middle(inputArr2), expectedOutput2);
+  });
+
+  it("check that array with even number of elements returns the two middle elements in an array", () => {
+    const inputArr1 = [1, 2, 3, 4];
+    const expectedOutput1 = [2, 3];
+    assert.deepEqual(middle(inputArr1), expectedOutput1);
+    
+    const inputArr2 = [1, 2, 3, 4, 5, 6];
+    const expectedOutput2 = [3, 4];
+    assert.deepEqual(middle(inputArr2), expectedOutput2);
+  });
+});
