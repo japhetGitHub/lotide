44d49bdb6c6b7ace5b2ef7091ccc187e24b12517
Japhet Elias <japhet9@gmail.com>
2021-09-21 19:06:19 +0000

    modularized takeUntil and converted tests to Mocha and Chai

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const takeUntil = function(array, callback) {
   // Returns a slice of the array with elements taken from the beginning. 
   // It should keep going until the callback/predicate returns a truthy value.
@@ -31,12 +11,4 @@ const takeUntil = function(array, callback) {
   return result;
 }
 
-//Test Cases
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood'])
+module.exports = takeUntil;
