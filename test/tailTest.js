70c8054eae17fadb0fdf6c060103b81abd2c250f
Japhet Elias <japhet9@gmail.com>
2021-09-21 05:29:39 +0000

    refactored tailTest.js to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,31 @@
-const assertEqual = require('../assertEqual');
+const chai = require('chai');
+const assert = chai.assert;
+
 const tail = require('../tail');
 
-//TEST CASES
+describe("#tail", () => {
+  it("check that the full tail is returned", () => {
+    const inputArr = ["Hello", "Lighthouse", "Labs"];
+    const expectedOutput = ["Lighthouse", "Labs"];
+    assert.deepEqual(tail(inputArr), expectedOutput);
+  });
 
-// Test Case: Check that the full tail is returned.
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
+  it("check that the original array is unchanged", () => {
+    const inputArr = ["Yo Yo", "Lighthouse", "Labs"];
+    const expectedOutput = 3;
+    tail(inputArr); // no need to capture the return value since we are not checking it
+    assert.strictEqual(inputArr.length, expectedOutput); // original array should still have 3 elements!
+  });
 
-// Test Case: Check that the original array is unchanged
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("check that a one element array yields an empty array as its tail", () => {
+    const inputArr = ["element"];
+    const expectedOutput = [];
+    assert.deepEqual(tail(inputArr), expectedOutput);
+  });
 
-// Test Case: Check that a one element array yields an empty array as its tail.
-const oneElemArr = ["element"];
-assertEqual(tail(oneElemArr).toString(), [].toString());
-
-// Test Case: Check that an empty array yields an empty array as its tail.
-assertEqual(tail([]).toString(), [].toString());
+  it("check that an empty array yields an empty array as its tail", () => {
+    const inputArr = [];
+    const expectedOutput = [];
+    assert.deepEqual(tail(inputArr), expectedOutput);
+  });
+});
